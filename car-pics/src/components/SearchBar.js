57e92289221a510c5718013297a2e074f814a88a
grzybowski1911 <grzybowski1911@gmail.com>
2020-12-01 15:54:23 -0800

    handling user input

---
+++ car-pics/src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends React.Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    render () {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" onChange={this.onInputChange}></input>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
